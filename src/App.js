6fa07f003d3b76470e6dd9ef0d0453e1a3fc58f2
Nacho-sf <serranof.ignacio@gmail.com>
2022-09-20 13:10:25 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
+import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
-import '../src/styles/styles.scss';
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom';
+
+import '../src/styles/styles.scss'
+
+import {userContext} from './context/userContext';
+
+
 
 function App() {
+
+  const [user, setUser] = useState("Guille"); // state para componente funcional
+
+  const login = (name) => {
+    setUser(name); // user = name. Ej: name="Guille" --> user = "Guille"
+  }
+
+  const logout = () => {
+    setUser(""); // user = ""
+  }
+
+  const data = {
+    user,
+    login,
+    logout
+  }
+
   return (
     <div className="App">
-      <Header/>
       <BrowserRouter>
-        <Main/>
+      <userContext.Provider value={data}>
+        <Header />
+        <Main />
+        </userContext.Provider>
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+
+import {userContext} from '../../../context/userContext';
 
 class Nav extends Component {
   render() {
@@ -9,6 +12,9 @@ class Nav extends Component {
         <Link to="/staff">Staff</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <userContext.Consumer>
+          {({logout,user})=> user?<span>Hola, {user} <Button variant="contained" size="small" onClick={logout} >LOGOUT</Button></span>:""}
+        </userContext.Consumer>
       </nav>;
     
   }
